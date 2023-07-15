ea955ac215209fb6545e585f3b6a884367e1dac5
MansiRavindraMohan <manasimohan29@gmail.com>
2023-07-15 20:13:33 +0530

    [Mansi]: Program for find minimum of 3 integers

---
+++ Day5_Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MinimumInteger minimumInteger = new MinimumInteger();
+            minimumInteger.IntegerMethod();
+            Console.ReadLine();
+        }
+    }
+}
